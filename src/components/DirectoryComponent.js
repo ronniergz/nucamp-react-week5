a8ac589cca76893f8fae8b72eb540fac7f86af66
Ronnie Rodriguez <rlrodrig88@gmail>
2020-01-08 22:11:46 -0600

    Functional Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 
-class Directory extends Component {
+function RenderDirectoryItem({campsite, onClick}) {
+  return (
+    <Card onClick={() => onClick(campsite.id)}>
+        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+        <CardImgOverlay>
+            <CardTitle>{campsite.name}</CardTitle>
+        </CardImgOverlay>
+    </Card>
+  );
+}
+
+function Directory(props) {
   
-    render() {
-      const directory = this.props.campsites.map(campsite => {
-          return (
-              <div key={campsite.id} className="col-md-5 m-1">
-                  <Card onClick={() => this.props.onClick(campsite.id)}>
-                      <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                      <CardImgOverlay>
-                          <CardTitle>{campsite.name}</CardTitle>
-                      </CardImgOverlay>
-                  </Card>
-              </div>
-          );
-      });
+
+  const directory = props.campsites.map(campsite => {
+    return (
+      <div key={campsite.id} className="col-md-5 m-1">
+        <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+      </div>
+    )
+  });
 
       return (
           <div className="container">
@@ -25,7 +31,7 @@ class Directory extends Component {
               </div>
           </div>
       );
-  }
+  
 }
 
 
