27bdbb5360b4b3082371a8828c033aefab4f8544
Ronnie Rodriguez <rlrodrig88@gmail>
2020-01-11 10:08:15 -0600

    React Router Parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDirectoryItem({campsite, onClick}) {
   return (
     <Card>
-        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-        <CardImgOverlay>
-            <CardTitle>{campsite.name}</CardTitle>
-        </CardImgOverlay>
+      <Link to={`/directory/${campsite.id}`}>
+          <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+          <CardImgOverlay>
+              <CardTitle>{campsite.name}</CardTitle>
+          </CardImgOverlay>
+      </Link>
     </Card>
   );
 }
@@ -36,7 +39,7 @@ function Directory(props) {
             {directory}
         </div>
     </div>
-);
+  );
   
 }
 
