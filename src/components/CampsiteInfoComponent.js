92ba7f798e827de284fb382c00aa69ffcca9243a
Ronnie Rodriguez <rlrodrig88@gmail>
2020-01-23 21:23:30 -0600

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -20,7 +20,7 @@ function RenderCampsite({campsite}) {
   )
 };
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -35,8 +35,7 @@ function RenderComments({comments}) {
             </div>
           )
         })}
-        
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   } return <div />;
@@ -72,8 +71,9 @@ class CommentForm extends Component {
   }  
 
   handleSubmitComment(values) {
-    alert(`Rating: ${values.rating} Your Name: ${values.author} Comment: ${values.text}`);
     this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    alert(`Rating: ${values.rating} Your Name: ${values.author} Comment: ${values.text}`);
   }
 
   validate(author) {
@@ -164,11 +164,16 @@ function CampsiteInfo (props) {
           </div>
           <div className="row">
               <RenderCampsite campsite={props.campsite} />
-              <RenderComments comments={props.comments} />
+              <RenderComments 
+                comments={props.comments}
+                addComment={props.addComment}
+                campsiteId={props.campsite.id}
+              />
           </div>
       </div>
   );   
-  } return <div />
+  } 
+  return <div />
 };
 
 
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,7 +1,14 @@
 import { COMMENTS } from '../shared/comments';
+import * as ActionTypes from './ActionTypes';
 
 export const Comments  = (state = COMMENTS, action) => {
   switch (action.type) {
+    case ActionTypes.ADD_COMMENT:
+      const comment = action.payload;
+      comment.id = state.length;
+      comment.date = new Date().toISOString();
+      //takes existing state, adds then returns new state to store
+      return state.concat(comment);  
     default:
       return state;
   }
